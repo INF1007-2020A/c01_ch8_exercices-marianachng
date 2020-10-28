814161ce3234434a6d2a39e15f113f2ab9aebd73
Mariana Chang Ieong <mariana.chang-ieong@polymtl.ca>
2020-10-28 13:20:52 -0400

    MCI: exercises update

--- exercice.py
+++ exercice.py
@@ -15,8 +15,8 @@ class ContextManagedList:
 # TODO: Importez vos modules ici
 
 # TODO: Définissez vos fonction ici
-def exercice1():
-    pass
+
+# EXERCICE 1: Écrire un programme qui compare le contenu de deux fichiers et signale la première différence rencontrée.
 def compare_files(nom_fichier_1: str, nom_fichier_2: str):
     with open(nom_fichier_1, "r") as f1, open(nom_fichier_2, "r") as f2:
         same = True
@@ -26,23 +26,36 @@ def compare_files(nom_fichier_1: str, nom_fichier_2: str):
             same = a == b
     return -1 if same else f1.tell()
 
-def exercice2():
-    pass
-    #Écrire un programme qui recopie un fichier
-    # texte en triplant tous les espaces entre les mots.
-    #(vous pouvez ouvrir deux fichiers avec l’instruction with)
+#EXERCICE 2: Écrire un programme qui recopie un fichier texte en triplant tous les espaces entre les mots.
+#(vous pouvez ouvrir deux fichiers avec l’instruction with)
 
 def triple_space(file1, file2):
     with open(file1, "r") as data, open(file1, "w") as result:
         text = data.read()
         new_text = text.replace(" ", "   ")
         result.write(new_text)
+#EXERCICE 3: Écrire un programme qui lit chaque ligne d’un fichier notes.txt (chaque ligne contient une note en pourcentage) et qui réécrit, dans un nouveau fichier, les notes avec, à coté, les mentions « A », « B », etc. en fonction d’un tableau de correspondance fourni.
+
+#EXERCICE 4: Reprenez l’exercice du livre de recettes et créer une base de données dans un fichier qui permet d’ajouter, modifier, supprimer des recettes. Vous êtes libre de choisir le type de format de fichier.
+
+#EXERCICE 5: Écrire un programme qui lit un fichier texte exemple.txt et retourne une liste de tous les nombres présents dans le fichier, en ordre croissant. 
+def find_numbers(file) -> list:
+    with open(file) as f:
+        text = f.read()
+        list_numbers = []
+        
+    return list_numbers
+#EXERCICE 6: Écrire un programme qui lit un fichier et qui recopie une ligne sur deux dans un autre fichier.
+def copy_lines(file1,file2):
+
+
+    pass
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    with ContextManagedList([1,2]) as my_list:
-        raise(Exception)
+    #with ContextManagedList([1,2]) as my_list:
+     #   raise(Exception)
 
     with open() as a, open() as b:
         pass
